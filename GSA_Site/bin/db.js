548be08d59ff36c5af8dadda1273fd01079168e4
Cobbleopolis <agounimon@yahoo.com>
2015-12-24 13:37:39 -0500

    Changed the site to use the dev user again

--- GSA_Site/bin/db.js
+++ GSA_Site/bin/db.js
@@ -1,13 +1,12 @@
 var mysql      = require('mysql');
 var profile = {
     host     : process.env.NODE_ENV === 'development' ? '45.16.76.67' : 'localhost',
-    user     : 'gsa-site',
+    user     : 'dev',
     password : 'gayisok1',
     database : 'gsa_site'
 };
 var connection = mysql.createConnection(profile);
 
-console.log(profile);
 module.exports.getDB = connection;
 
 //module.exports = connection;
